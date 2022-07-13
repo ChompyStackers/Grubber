a1624716ec37178544610f016968c814ba8eb813
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-07-13 10:10:44 -0700

    Footer completed

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 import PropTypes from "prop-types"
 import {
   BrowserRouter as  Router,
@@ -12,7 +13,9 @@ class App extends React.Component {
     return (
       <React.Fragment>
       <Header/>
+      <Footer/>
       </React.Fragment>
+
     );
   }
 }
