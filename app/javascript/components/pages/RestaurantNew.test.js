5bc82cffa2da15ed8d55ea2add162f207ea3e159
SOMarrah <stephenomarrah@gmail.com>
2022-07-14 14:15:36 -0700

    did some testing updates.

---
+++ app/javascript/components/pages/RestaurantNew.test.js
@@ -0,0 +1,26 @@
+// Imports React into our test file.
+import React from 'react'
+
+// Imports Enzyme testing and deconstructs Shallow into our test file.
+import Enzyme, { shallow } from 'enzyme'
+ 
+// Imports Adapter utilizing the latest react version into our test file so we can run a testing render on any component we may need.
+import Adapter from 'enzyme-adapter-react-16'
+
+// Imports in the component we are going to be testing.
+import RestaurantNew from './RestaurantNew'
+
+//Allows us to utilize the adapter we import in earlier, allowing us to call and render a component.
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("When RestaurantNew renders", () => {
+
+    let newRender
+    beforeEach(() => {
+      newRender = shallow(<RestaurantNew/>)
+    })
+    it("displays a form", () => {
+      const form = newRender.find("Form")
+      expect(form.length).toEqual(1)
+    })
+})
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  it "should validate city" do
-    restaurant = Restaurant.create
-    expect(restaurant.errors[:city]).to_not be_empty
+  user = User.where(email: 'test@example.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+  describe 'presence' do
+      it 'validates precense of name' do
+          restaurant =user.restaurants.create(
+                    name: "",
+                    street:"123 street",
+                    city: "Chicago",
+                    state: "Illinois",
+                    comment: "hello",
+                    foodtype: "chinese",
+                    image: "www.urlhelper.com"
+          )
+          p restaurant.errors[:name]
+          expect(restaurant.errors[:name]).to_not be_empty
+      end
   end
 end
+
+
