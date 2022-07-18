dbdcda20ce8b08050c12f28ee2ac6079d14ac358
F-Rivers <francisco.rivera92@icloud.com>
2022-07-18 13:39:39 -0700

    added test for protected index

--- app/javascript/components/pages/RestaurantIndexProtected.test.js
+++ app/javascript/components/pages/RestaurantIndexProtected.test.js
@@ -3,20 +3,20 @@ import React from 'react'
 
 // Imports Enzyme testing and deconstructs Shallow into our test file.
 import Enzyme, { shallow } from 'enzyme'
-
+ 
 // Imports Adapter utilizing the latest react version into our test file so we can run a testing render on any component we may need.
 import Adapter from 'enzyme-adapter-react-16'
 
 // Imports in the component we are going to be testing.
-import AboutUs from './AboutUs'
+import RestaurantIndexProtected from './RestaurantIndexProtected'
 
 //Allows us to utilize the adapter we import in earlier, allowing us to call and render a component.
-Enzyme.configure({adapter: new Adapter()})
+Enzyme.configure({ adapter: new Adapter() })
 
-describe("When AboutUs renders", () => {
-    it("displays a card group", () => {
-      const aboutus = shallow(<AboutUs />)
-      const aboutusHeading = aboutus.find("div")
-      expect(aboutusHeading.text()).toEqual("AboutUs<CardGroup />")
+describe("When restaurantProtectedIndex renders", () => {
+    it("displays the user restaurant card", () => {
+      const protecindex= shallow(<RestaurantIndexProtected/>)
+      const protecindexHeader = protecindex.find("div")
+      expect(protecindexHeader.length).toEqual(1)
     })
-  })
+})
