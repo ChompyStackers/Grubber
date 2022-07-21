5ae6d988b7babcc0def71073e9dcb2f2faa690ee
LoneWanderer101 <sgtlinares@gmail.com>
2022-07-21 13:33:28 -0700

    started working on user_spec testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.where(email: 'test@example.com').first_or_create(encrypted_password: '12345678')
+  describe 'presence' do
+    it 'validates precense of email' do
+      user =user.created_at(
+        email: "",
+        encrypted_password:"123 street"
+      )
+      p user.errors[:email]
+      expect(user.errors[:email]).to_not be_empty
+    end
+    it 'validates precense of encrypted password' do
+      user =user.created_at(
+        email: "me@gmail.com",
+        encrypted_password: ""
+      )
+      p user.errors[:encrypted_password]
+      expect(user.errors[:encrypted_password]).to_not be_empty
+    end
+  end 
 end
