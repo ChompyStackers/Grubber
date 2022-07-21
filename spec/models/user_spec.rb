f8f26e0cd865502927ee1275077b6c5f59baad41
LoneWanderer101 <sgtlinares@gmail.com>
2022-07-21 14:50:20 -0700

    testing on user.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  user = User.where(email: 'test@example.com').first_or_create(encrypted_password: '12345678')
   describe 'presence' do
-    it 'validates precense of email' do
-      user =user.created_at(
-        email: "",
-        encrypted_password:"123 street"
-      )
+    it 'validates presence of email' do
+      user = User.where(email: '').first_or_create(password: '12345678', password_confirmation: '12345678')
       p user.errors[:email]
       expect(user.errors[:email]).to_not be_empty
     end
-    it 'validates precense of encrypted password' do
-      user =user.created_at(
-        email: "me@gmail.com",
-        encrypted_password: ""
-      )
-      p user.errors[:encrypted_password]
-      expect(user.errors[:encrypted_password]).to_not be_empty
+    it 'validates presence of password' do
+      user = User.where(email: '').first_or_create(password: '', password_confirmation: '12345678')
+      p user.errors[:password]
+      expect(user.errors[:password]).to_not be_empty
+    end
+    it 'validates presence of password_confirmation' do
+      user = User.where(email: '').first_or_create(password: '12345678', password_confirmation: '1234567')
+      p user.errors[:password_confirmation]
+      expect(user.errors[:password_confirmation]).to_not be_empty
     end
   end 
 end
