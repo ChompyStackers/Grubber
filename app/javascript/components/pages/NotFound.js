5ba796d8bb5e372134e5df31d6299d5c781def74
SOMarrah <stephenomarrah@gmail.com>
2022-07-25 10:25:07 -0700

    notfoundpage

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -5,6 +5,9 @@ export default class NotFound extends Component {
   render() {
     return (
           <div className='homeCardContainer' id='notFound'>
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
             <div className='notFoundContainer'>THIS IS NOT THE GRUB YOU ARE LOOKING FOR...
               <div>
                 <Spinner>
